1abc71d50a8607044adaceb61a15602172e72cd8
LeskaB <abejaran@stumail.jccc.edu>
2025-04-25 23:16:38 -0500

    Add files via upload

---
+++ Project3/whatever.js
@@ -0,0 +1,12 @@
+let name = "Aksel Bejarano";
+let salary = 35000;
+let vet = false;
+let friends = new Array("Elena", "Abraham", "Finn");
+let friendsal = new Array(50000, 100000, 45000)
+const friend4 = {
+    firstName: 'John',
+    lastName: "Doe",
+    salary: 3000000
+ };
+
+ console.log(`Name:${name} Salary:${salary} Vet: ${vet} Friends:${friends} Friend Salary ${friendsal} Extra:${friend4.firstName} ${friend4.lastName} ${friend4.salary}`)
